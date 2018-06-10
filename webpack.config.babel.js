2d7b66ee1e756362fc8f7130ae6e07602800f958
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2018-06-09 21:39:59 -0500

    build: no obfuscator

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,6 @@
 import { resolve } from 'path'
 import webpack from 'webpack'
 import nodeExternals from 'webpack-node-externals'
-import JavaScriptObfuscator from 'webpack-obfuscator'
 
 export default ({ production } = {}) => ({
   entry: {
@@ -23,7 +22,6 @@ export default ({ production } = {}) => ({
     ]
   },
   plugins: [
-    production && new JavaScriptObfuscator(),
     new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })
   ].filter(Boolean)
 })
