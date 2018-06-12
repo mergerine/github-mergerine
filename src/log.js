f72ad405f6e1836430b377506db4ad28188baa76
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2018-06-12 00:53:42 -0500

    feat: single-line logging in production

--- src/log.js
+++ src/log.js
@@ -1,22 +1,32 @@
 import debug from 'debug'
 import { inspect } from 'util'
 
-const log = (...data) =>
-  debug('mergerine:log')(
-    ...data.map(datum =>
-      inspect(datum, {
-        colors: true,
-        depth: null,
-        maxArrayLength: null
-      })
-    )
+const { NODE_ENV } = process.env
+
+const stringify = args =>
+  args.map(arg => (typeof arg === 'string' ? arg : JSON.stringify(arg)))
+
+const inspectify = args =>
+  args.map(arg =>
+    inspect(arg, {
+      colors: true,
+      depth: null,
+      maxArrayLength: null
+    })
   )
 
-const trace = debug('mergerine:trace')
-const logRun = debug('mergerine:run')
-const logFetchOk = debug('mergerine:fetch:ok')
-const logFetchErr = debug('mergerine:fetch:err')
-const logDecide = debug('mergerine:decide')
+const debugOneLine = label => {
+  const dbg = debug(label)
+  return (...args) =>
+    dbg(...(NODE_ENV === 'production' ? stringify(args) : inspectify(args)))
+}
+
+const log = debugOneLine('mergerine:log')
+const trace = debugOneLine('mergerine:trace')
+const logRun = debugOneLine('mergerine:run')
+const logFetchOk = debugOneLine('mergerine:fetch:ok')
+const logFetchErr = debugOneLine('mergerine:fetch:err')
+const logDecide = debugOneLine('mergerine:decide')
 
 export { log, logRun, logFetchOk, logFetchErr, logDecide, trace }
 
